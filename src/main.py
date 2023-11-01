a46adbe89ebff7f1710f60f5dc4fb1128ffbfd58
Binary Lynx <binarylynx01@gmail.com>
2023-11-01 17:25:51 +0300

    Добавлен базовый дефолтный ендпоинт

--- src/main.py
+++ src/main.py
@@ -4,3 +4,8 @@ from src.routers import notification
 
 app = FastAPI()
 app.include_router(notification.router)
+
+
+@app.get("/")
+def go_reroute_yourslef():
+    return "visit '/docs' to view available routes"
